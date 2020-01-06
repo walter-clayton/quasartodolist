fa6cd5f472ca533a5d176a2c15436ea4ca6c87c9
Walter Clayton <walthedude@gmail.com>
2020-01-06 08:56:11 +0100

    first commit

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,99 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="bg-grey-10 q-mx-none">
+        <q-btn class="q-m-none"
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          icon="menu"
+          aria-label="Menu"
+        />
+          <div class=" q-py-xl q-sb-md q-mx-auto">
+              <div class="text-h3">Todo</div>
+          <div class="text-subtitle q-py-sm q-mr-none">{{todaysDate}}</div>
+          </div>
+      </q-toolbar>
+    </q-header>
+
+      <q-drawer
+        v-model="leftDrawerOpen"
+        show-if-above
+        :width="200"
+        :breakpoint="600"
+      >
+        <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item
+            to="/"
+            clickable
+            v-ripple>
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+              <q-item-section>
+                Todo
+              </q-item-section>
+            </q-item>
+            <q-item
+            to="/help"
+            clickable
+            v-ripple>
+              <q-item-section avatar>
+                <q-icon name="help" />
+              </q-item-section>
+              <q-item-section>
+                Help
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="https://images.unsplash.com/photo-1496964551482-5eef1122d121?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80" style="height: 150px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+            <div class="text-weight-bold">Walter Clayton</div>
+            <div>@walthedude</div>
+          </div>
+        </q-img>
+      </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { date } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  },
+  computed: {
+    todaysDate () {
+      let timeStamp = Date.now()
+      return date.formatDate(timeStamp, 'dddd M MMMM YYYY')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .header-image {
+    height: 100%;
+    z-index: -1;
+    opacity: 0.5;
+    filter: grayscale(100%);
+  }
+</style>
---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis assumenda velit eaque earum, id obcaecati dolores officia. Fuga vero nisi nam iure aliquid similique architecto repudiandae! Reiciendis provident ea repellendus.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis assumenda velit eaque earum, id obcaecati dolores officia. Fuga vero nisi nam iure aliquid similique architecto repudiandae! Reiciendis provident ea repellendus.</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
